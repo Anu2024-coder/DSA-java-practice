a078bf10a8593fe971eff396cd220b8bb5bdf53f
Anu gill <gillanu2003@gmail.com>
2026-01-21 23:39:04 +0530

    learning strings

---
+++ Strings/toUpperCase.java
@@ -0,0 +1,22 @@
+public class toUpperCase {
+    public static String toUppperCase(String str){
+        StringBuilder sb=new StringBuilder(" ");
+
+        sb.append(Character.toUpperCase(str.charAt(0)));
+
+        for(int i=1;i<str.length();i++){
+            if(str.charAt(i)==' ' && i<str.length()-1){
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            }else{
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String args[]){
+        String str= "hey hi how are you";
+        System.out.println(toUppperCase(str));
+    }
+}
