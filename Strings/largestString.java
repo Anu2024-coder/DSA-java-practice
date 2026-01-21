70f2db3ca63a48a0e0faf02d37cc4b36d56cfe3d
Anu gill <gillanu2003@gmail.com>
2026-01-21 23:12:19 +0530

    learning strings

---
+++ Strings/largestString.java
@@ -0,0 +1,15 @@
+public class largestString {
+    public static String largest(String str[]){
+        String largest=str[0];
+        for(int i=1;i<str.length;i++){
+            if(largest.compareTo(str[i])<0){
+                largest=str[i];
+            }
+        }
+        return largest;
+    }
+    public static void main(String args[]){
+        String fruits[]={"apple","mango","banana"};
+        System.out.println(largest(fruits));
+    }
+}
--- Strings/string.java
+++ Strings/string.java
@@ -6,6 +6,14 @@ public class string{
         }
         System.out.println();
     }
+
+    public static String substring(String str, int si, int ei){
+        String substring="";
+        for(int i=si; i<ei;i++){
+            substring+=str.charAt(i);
+        }
+        return substring;
+    }
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         String str="abcd";
@@ -22,6 +30,34 @@ public class string{
         System.out.println(full); 
         System.out.println(name.charAt(0));//charAt method
         printLetters(name);
+
+        //compare
+        String s1="Anu";
+        String s2="Anu";
+        String s3=new String("Anu");
+
+        if(s1==s2){
+            System.out.println("Strings are equal");
+        }else{
+            System.out.println("Strings are not equal");
+        }
+
+        if(s1==s3){
+            System.out.println("Strings are equal");
+        }else{
+            System.out.println("Strings are not equal");
+        }
+
+        if(s1.equals(s3)){
+            System.out.println("Strings are equal");
+        }
+
+        //substring
+        String strrr="Anu gill";
+        System.out.println(substring(strrr,0,5));
+
+        //built in substring function
+        System.out.println(strrr.substring(0,5));
         sc.close();
     }
 }
