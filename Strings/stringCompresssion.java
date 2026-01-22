a3e421886b9c3254862a3a93345ff7f8e29ec828
Anu gill <gillanu2003@gmail.com>
2026-01-22 11:56:28 +0530

    string completed

---
+++ Strings/stringCompresssion.java
@@ -0,0 +1,22 @@
+public class stringCompresssion {
+    public static String compressString(String str){
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<str.length();i++){
+            Integer count =0;
+            while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            sb.append(str.charAt(i));
+            if(count>1){
+                sb.append(count.toString());
+            }
+        }
+        return sb.toString();
+
+    }
+    public static void main(String args[]){
+        String str="aaabbbbbcccd";
+        System.out.println(compressString(str));
+    }
+}
