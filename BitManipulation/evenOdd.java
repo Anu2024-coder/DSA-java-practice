f8f926e9197e7607b187a5d919831f031c0b92df
Anu gill <gillanu2003@gmail.com>
2026-01-29 15:20:25 +0530

    bit manipulation

--- BitManipulation/evenOdd.java
+++ BitManipulation/evenOdd.java
@@ -7,8 +7,30 @@ public class evenOdd {
             System.out.println("odd");
         }
     }
+
+    public static int getIthBit(int n, int i){
+        int bitmask=1<<i;
+        if((n & bitmask)==0){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+
+    public static int setIthBit(int n, int i){
+        int bitmask=1<<i;
+        return n | bitmask;
+    }
+
+    public static int clearIthBit(int n, int i){
+        int bitmask=~(1<<i);
+        return n&bitmask;
+    }
     public static void main(String args[]){
         checkEvenOdd(3);
         checkEvenOdd(14);
+        System.out.println(getIthBit(10,2));
+        System.out.println(setIthBit(10,2));
+        System.out.println(clearIthBit(10,1));
     }
 }
