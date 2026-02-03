e3bf40b0c18d831cacd69037a47078b86fbe2955
Anu gill <gillanu2003@gmail.com>
2026-02-03 15:56:19 +0530

    learnign sorting- merge sort

--- BitManipulation/evenOdd.java
+++ BitManipulation/evenOdd.java
@@ -26,11 +26,67 @@ public class evenOdd {
         int bitmask=~(1<<i);
         return n&bitmask;
     }
+
+    public static int updateIthBit(int n, int i, int newBit){
+        // if(newBit==0){
+        //     return clearIthBit(n,i);
+        // }else{
+        //     return setIthBit(n,i);
+        // }
+        n=clearIthBit(n,i);
+        int bitmask=newBit<<i;
+        return n | bitmask;
+        }
+
+    public static int clearIBits(int n, int i){
+            int bitmask= (~0)<<i;
+            return n&bitmask;
+            }
+
+    public static int clearItoJBits(int n, int i, int j){
+        int a=(~0)<<(j+1);
+        int b=(1<<i)-1;
+        int bitmask= a|b;
+        return n&bitmask;
+    }
+
+    public static boolean isPowerOf2(int n){
+        return (n&(n-1))==0;
+    }
+
+    public static int countSetBits(int n){
+        int count = 0;
+        while(n>0){
+            if((n&1)!=0){
+                count++;
+            }
+            n=n>>1;
+        }
+        return count;
+    }
+
+    public static int fastExpo(int a,int n){
+        int ans=1;
+        while(n>0){
+            if((n&1)!=0){
+                ans=ans*a;
+            }
+            a=a*a;
+            n=n>>1;
+        }
+        return ans;
+    }
     public static void main(String args[]){
         checkEvenOdd(3);
         checkEvenOdd(14);
         System.out.println(getIthBit(10,2));
         System.out.println(setIthBit(10,2));
         System.out.println(clearIthBit(10,1));
+        System.out.println(updateIthBit(10,2,1));
+        System.out.println(clearIBits(15,2));
+        System.out.println(clearItoJBits(10,2,7));
+        System.out.println(isPowerOf2(8));
+        System.out.println(countSetBits(10));
+        System.out.println(fastExpo(10,2));
     }
 }
