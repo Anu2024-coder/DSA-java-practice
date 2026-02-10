76451af6d5b6d13f038eee611f042798db143d65
Anu gill <gillanu2003@gmail.com>
2026-02-10 16:43:38 +0530

    learning trees

--- sorting/inbuiltSort.java
+++ sorting/inbuiltSort.java
@@ -1,17 +1,17 @@
 import java.util.Arrays;
 import java.util.Collections;
 public class inbuiltSort {
-    public static void printArr(int arr[]){
+    public static void printArr(Integer arr[]){
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
         System.out.println();
     }
     public static void main(String args[]){
-        int arr[]={1,3,57,90,4,4};
-        Integer arr1[]={3,574,2466,13,3,53,5};
-        Arrays.sort(arr,0,5);
-        Arrays.sort(arr1,0,5,Collections.reverseOrder());
-        printArr(arr);
+        //int arr[]={1,3,57,90,4,4};
+        Integer arr1[]={574,2466,3,13,3,53,5};
+        //Arrays.sort(arr,0,5);
+        Arrays.sort(arr1,0,3,Collections.reverseOrder());
+        printArr(arr1);
     }
 }
