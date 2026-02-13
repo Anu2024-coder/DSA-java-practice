912d8038d38897128af2958fe930874c4d9ab310
Anu gill <gillanu2003@gmail.com>
2026-02-13 13:17:10 +0530

    subtree

--- Trees/Binary trees/heightOfTree.java
+++ Trees/Binary trees/heightOfTree.java
@@ -50,6 +50,28 @@ public class heightOfTree {
         int selfD=lh+rh+1;
         return Math.max(selfD, Math.max(ld,rd));
     }
+
+    static class Info{
+        int diam;
+        int ht;
+
+        public Info(int diam, int ht){
+            this.diam=diam;
+            this.ht=ht;
+        }
+    }
+
+    public static Info diameter2(Node root){
+        if(root==null){
+            return new Info(0,0);
+        }
+        Info leftInfo=diameter2(root.left);
+        Info rightInfo=diameter2(root.right);
+
+        int diam=Math.max(Math.max(leftInfo.diam, rightInfo.diam), leftInfo.ht+rightInfo.ht+1);
+        int ht=Math.max(leftInfo.ht,rightInfo.ht)+1;
+        return new Info(diam,ht);
+    }
     public static void main(String args[]){
         Node root = new Node(1);
         root.left=new Node(2);
@@ -62,5 +84,7 @@ public class heightOfTree {
         System.out.println(count(root));
         System.out.println(sum(root));
         System.out.println(diameter(root));
+        System.out.println(diameter2(root).diam);
+        System.out.println(diameter2(root).ht);
     }
 }
