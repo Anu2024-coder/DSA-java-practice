7f42b12ac47ed58066686a5db7e95dab80be5463
Anu gill <gillanu2003@gmail.com>
2026-02-27 11:17:17 +0530

    learning Stack

---
+++ Trees/Binary trees/BST/sizeOfLargestBSTInBT.java
@@ -0,0 +1,15 @@
+public class sizeOfLargestBSTInBT {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data=data;
+            this.left=this.right=null;
+        }
+        public static void main(String args[]){
+            
+        }
+    }
+}
