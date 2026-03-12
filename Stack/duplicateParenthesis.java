6a01869a7d11a71677fb6472cd96540cf8db55e7
Anu gill <gillanu2003@gmail.com>
2026-03-12 13:45:00 +0530

    duplicate parenthesis

---
+++ Stack/duplicateParenthesis.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class duplicateParenthesis{
+    public static boolean isDuplicate(String str){
+        Stack<Character> s=new Stack<>();
+        int n=str.length();
+        for(int i=0;i<n;i++){
+            char ch=str.charAt(i);
+            //closing
+            if(ch==')'){
+                int count=0;
+                while(s.peek()!='('){
+                    s.pop();
+                    count++;
+                }
+                if(count<1){
+                    return true; //duplicate
+                }else{
+                    s.pop(); //opening pair
+                }
+            }else{
+                //opening
+                s.push(ch);
+            }
+        }
+        return false;
+    }
+    public static void main(String args[]){
+        String str="((a+b))";
+        String str2="(a-b)";
+        System.out.println(isDuplicate(str2));
+    }
+}
