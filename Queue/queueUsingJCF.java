fe3a808fc377a550a17c3b4eeb1f8b9fa6ef63bc
Anu gill <gillanu2003@gmail.com>
2026-03-16 08:16:14 +0530

    queue using java collection framework

---
+++ Queue/queueUsingJCF.java
@@ -0,0 +1,15 @@
+//Queue is an interface not a class, we can't create objects of interfaces  that's why we use linkedList or ArrayDeque class to implement queue
+import java.util.*;
+public class queueUsingJCF {
+    public static void main(String args[]){
+        Queue<Integer> q= new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
