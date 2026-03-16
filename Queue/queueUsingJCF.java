d98629a513dee1dbdce70edf8b2f54ea213033d2
Anu gill <gillanu2003@gmail.com>
2026-03-16 08:33:58 +0530

    queue using two stacks

--- Queue/queueUsingJCF.java
+++ Queue/queueUsingJCF.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class queueUsingJCF {
     public static void main(String args[]){
         Queue<Integer> q= new LinkedList<>();
+       //Queue<Integer>q=new ArrayDeque<>();
         q.add(1);
         q.add(2);
         q.add(3);
